4915db138a2606e459a1af7a5f05e59bf6654a65
Shahid <shaahid.xd@gmail.com>
2020-01-10 23:19:20 +0530

    First commit

---
+++ src/main/java/com/fashionista/api/FashionistaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fashionista.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionistaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionistaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fashionista/api/FashionistaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashionista.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionistaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
